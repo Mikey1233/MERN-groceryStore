db887c6758a269ecd4b64dbcae4be6d509d31b83
Michael Emmanuel <emmamichael201@gmail.com>
2025-07-12 06:49:09 +0100

    Remove fallback localhost URL from axios instance baseURL configuration

--- client/lib/axios.ts
+++ client/lib/axios.ts
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: `${process.env.BASE_URL}/api` ||'http://localhost:5000/api',
+  baseURL: `${process.env.BASE_URL}/api`,
   headers: {
     'Content-Type': 'application/json',
   },
