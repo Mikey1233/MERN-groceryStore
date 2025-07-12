f3f77a6819aaf317e9eb869a88b53350d62b08e2
Michael Emmanuel <emmamichael201@gmail.com>
2025-07-12 06:37:22 +0100

    Update axios instance and CORS configuration to use environment variables for base URL and frontend site

--- server/app.js
+++ server/app.js
@@ -5,7 +5,7 @@ const imageUploadRoutes = require("./routes/cloudinaryRoute")
 const app = express()
 const cors = require("cors")
 const corsOptions = {
-  origin:process.env.FRONTEND_SITE || 'http://localhost:3000', // frontend URL
+  origin:process.env.FRONTEND_SITE, // frontend URL
    methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true, // allow cookies or authorization headers
 };
