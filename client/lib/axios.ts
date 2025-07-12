df7cb7d43578b6603b64dc3c12c5a6272d0f6ddf
Michael Emmanuel <emmamichael201@gmail.com>
2025-07-12 07:25:50 +0100

    Update axios instance baseURL to use NEXT_PUBLIC_BACKEND environment variable

--- client/lib/axios.ts
+++ client/lib/axios.ts
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL:  `${process.env.BACKEND}/api`,
+  baseURL:  `${process.env.NEXT_PUBLIC_BACKEND}/api`,
   headers: {
     'Content-Type': 'application/json',
   },
